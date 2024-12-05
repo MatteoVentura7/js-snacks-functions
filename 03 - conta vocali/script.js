72718aceec430339567748fe0fcc9d6fbe8c5ed8
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-05 16:58:02 +0100

    add : 03-conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const word2 = (prompt("inserisci una parola"))
 
 // Dichiara la funzione qui.
 
+function stringainiziale(stringavocali){ 
+    const vocali = 'aeiou'  
+    let wordvocal = 0;
+    for(let i = 0; i < stringavocali.length; i++){
+      if(vocali.indexOf(stringavocali[i]) !== -1){
+        wordvocal += 1;
+      }
+    }
+    return wordvocal;
+  }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(stringainiziale(word));
+console.log(stringainiziale(word2));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
