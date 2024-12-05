e4e7edefb0add4858e3e567662a1741a3c4395b4
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-05 15:59:04 +0100

    add : 01-iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function startLetters (array) {
+    let newArray = [];
+    
+    for (let i = 0; i < names.length; i++){
+        let word = names[i]; 
+        newArray.push(word.substring(0,1));  
+    }
+    return newArray
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const iniziali = (startLetters(names));
+console.log(iniziali); 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
